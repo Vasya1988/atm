81e6aaa3e3c680382e039e702a36732efca57f40
Vasily_Romanov <61226796+Vasya1988@users.noreply.github.com>
2023-07-05 16:31:32 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
     output: "export",
     // Optional: Add a trailing slash to all paths `/about` -> `/about/`
-    // trailingSlash: true,
+    trailingSlash: true,
     // Optional: Change the output directory `out` -> `dist`
     // distDir: 'dist',
     // images: {
