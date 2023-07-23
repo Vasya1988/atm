b7cf22b466d583b4d66a65b8e7d7d9233e095e34
Vasily Romanov <va5ily@MacBook-Air-Vasily.local>
2023-07-23 21:27:48 +0300

    add count function

--- app/atm/page.js
+++ app/atm/page.js
@@ -1,6 +1,22 @@
 
 const Atm = ( {children}) => {
 
+    const getMoney = (amount, 
+        banknotes = [
+            {banknote: 1000, amount: 4, status: 'ready'},
+            {banknote: 500, amount: 4, status: 'ready'},
+            {banknote: 100, amount: 4, status: 'ready'},
+            {banknote: 50, amount: 4, status: 'ready'},
+            {banknote: 20, amount: 4, status: 'ready'},
+            {banknote: 10, amount: 4, status: 'ready'}
+        ]
+    ) => {
+        const currentBanknotes = []
+        banknotes.map(el => currentBanknotes.push(
+            {banknote: el.banknote, amount: el.amount, status: el.status}
+        ))
+    }
+
     
     return (
         <main>{children}</main>
